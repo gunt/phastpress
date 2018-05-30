e838b7812c84cadce4c4febcaa0a1ae4d299f026
Albert Peschar <albert@peschar.net>
2018-05-30 11:32:03 +0300

    Add php52.phastpress.test

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,12 +8,28 @@ while ! mysql -hdb -uroot -pwordpress -e 'select 1;' >/dev/null; do
     sleep 1
 done
 
+if [ -f index.html ]; then
+    rm -f index.html
+fi
+
+if [ ! -f index.php ]; then
+    wp core download
+fi
+
 if [ ! -f wp-config.php ]; then
     wp config create \
         --dbhost=db \
         --dbname=wordpress \
         --dbuser=wordpress \
-        --dbpass=wordpress
+        --dbpass=wordpress \
+        --extra-php <<EOF
+define('WP_SITEURL',
+    isset(\$_SERVER['HTTP_HOST']) ?
+    'http://' . \$_SERVER['HTTP_HOST'] :
+    'http://phastpress.test'
+);
+define('WP_HOME', WP_SITEURL);
+EOF
 fi
 
 wp core install \
