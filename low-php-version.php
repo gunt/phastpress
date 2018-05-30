0a59f16f2d2bcde5bdc31e6a70d05b12419ae221
Albert Peschar <albert@peschar.net>
2018-05-30 09:55:12 +0300

    Fix PHP 5.2 syntax compat in low-php-version.php

--- low-php-version.php
+++ low-php-version.php
@@ -1,10 +1,12 @@
 <?php
 
-add_action('wp_ajax_phastpress_get_admin_panel_data', function () {
+function phastpress_low_php_version_data() {
     wp_send_json(array(
         'error' => array(
             'type' => 'low-php',
             'version' => PHP_VERSION
         )
     ));
-});
+}
+
+add_action('wp_ajax_phastpress_get_admin_panel_data', 'phastpress_low_php_version_data');
